875d93b68a7226a7a2fefafa0719e044edae54e4
Vivi Jiang <mobojiang@gmail.com>
2014-10-02 22:37:46 +0800

    false scope in directive

--- js/demo.test.js
+++ js/demo.test.js
@@ -4,7 +4,9 @@
 var demoForm = angular.module('demo.test', ['odin.ui']);
 demoForm.controller('demoTestController',function($scope, $element){
 	$scope.title='node title';
-
+	$scope.setAppTitle =function(title){
+				$scope.title=title;
+			};
 });
 
 })(jQuery);
